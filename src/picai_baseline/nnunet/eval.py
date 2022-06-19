6ec4c2453b520d8603a14e5daf222d9c1bf1a39c
Joeran <joeran@bosma.co>
2022-06-19 11:28:14 +0200

    Cleanup

--- src/picai_baseline/nnunet/eval.py
+++ src/picai_baseline/nnunet/eval.py
@@ -6,33 +6,44 @@ from picai_prep.preprocessing import crop_or_pad
 from report_guided_annotation import extract_lesion_candidates
 
 
-def crop(img):
+def nnUNet_cropped_prediction_compatibility(img):
     return crop_or_pad(img, size=(20, 320, 320))
+
+
+def extract_lesion_candidates_compatibility(pred):
+    return nnUNet_cropped_prediction_compatibility(extract_lesion_candidates(pred)[0])
 
 
 task = "Task2201_picai_baseline"
 trainer = "nnUNetTrainerV2_Loss_FL_and_CE_checkpoints__nnUNetPlansv2.1"
-fold = 0
 results_dir = Path("/workdir/results/nnUNet/3d_fullres/")
 
-for epoch in range(50, 600+1, 50):
-    checkpoint = f"model_ep_{epoch:03d}"
-    softmax_dir = results_dir / task / trainer / f"fold_{fold}/picai_pubtrain_predictions_{checkpoint}"
-    metrics_path = softmax_dir / "metrics.json"
+for fold in range(5):
+    print(f"Fold {fold}")
+    if fold == 0:
+        epochs = list(range(50, 950+1, 50)) + ["model_best", "model_final_checkpoint"]
+    else:
+        epochs = ["model_best"]
 
-    if metrics_path.exists():
-        print(f"Metrics found at {metrics_path}, skipping..")
+    for epoch in epochs:
+        checkpoint = f"model_ep_{epoch:03d}"
+        softmax_dir = results_dir / task / trainer / f"fold_{fold}/picai_pubtrain_predictions_{checkpoint}"
+        metrics_path = softmax_dir / "metrics.json"
 
-    # evaluate
-    metrics = evaluate_folder(
-        y_det_dir=softmax_dir,
-        y_true_dir=f"/workdir/nnUNet_raw_data/{task}/labelsTr",
-        subject_list=valid_splits[fold]['subject_list'],
-        y_det_postprocess_func=lambda pred: crop(extract_lesion_candidates(pred)[0]),
-        y_true_postprocess_func=crop,
-    )
+        if metrics_path.exists():
+            print(f"Metrics found at {metrics_path}, skipping..")
+            continue
 
-    # save and show metrics
-    metrics.save(metrics_path)
-    print(f"Results for epoch {epoch}:")
-    print(metrics)
+        # evaluate
+        metrics = evaluate_folder(
+            y_det_dir=softmax_dir,
+            y_true_dir=f"/workdir/nnUNet_raw_data/{task}/labelsTr",
+            subject_list=valid_splits[fold]['subject_list'],
+            y_det_postprocess_func=extract_lesion_candidates_compatibility,
+            y_true_postprocess_func=nnUNet_cropped_prediction_compatibility,
+        )
+
+        # save and show metrics
+        metrics.save(metrics_path)
+        print(f"Results for epoch {epoch}:")
+        print(metrics)
