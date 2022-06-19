b1feb8b748d058092656a73796dd046171ca0969
Joeran Bosma <joeran@bosma.co>
2022-06-19 16:29:42 +0200

    Cleanup UNet training script (#4)
    * Changes to train SwinUNETR
    
    * Cleanup UNet training script
    
    - Follow PEP8 style guides, which also allows to track changes (instead of many lines changing due to indentation, as is happening in this commit)
    - Capitalise sentences and proper nouns, not every word
    - Change image_size to normal list of ints
    
    * Revert "Merge branch 'main' into unet-cleanup"
    
    This reverts commit 2692a8f64ddbcf3f677071669f960cc17fb2676d, reversing
    changes made to 24fddd972235dd78199eed85e4e0f0d1e9ed8173.
    
    * Improve error handling network selector
    
    * Fix small typo + add a bit more clarity
    
    Co-authored-by: anindo-saha <anindo@ieee.org>

--- src/picai_baseline/unet/training_setup/neural_network_selector.py
+++ src/picai_baseline/unet/training_setup/neural_network_selector.py
@@ -18,12 +18,17 @@ from picai_baseline.unet.training_setup.neural_networks.unets import UNet
 def neural_network_for_run(args, device):
     """Select neural network architecture for given run"""
 
-    if (args.model_type == 'unet'):
-        model = UNet(spatial_dims=len(args.image_shape),
-                     in_channels=args.num_channels,
-                     out_channels=args.num_classes,
-                     strides=args.model_strides,
-                     channels=args.model_features).to(device)
+    if args.model_type == 'unet':
+        model = UNet(
+            spatial_dims=len(args.image_shape),
+            in_channels=args.num_channels,
+            out_channels=args.num_classes,
+            strides=args.model_strides,
+            channels=args.model_features
+        )
+    else:
+        raise ValueError(f"Unknown model type: {args.model_type}")
 
+    model = model.to(device)
     print("Loaded Neural Network Arch.:", args.model_type)
     return model
