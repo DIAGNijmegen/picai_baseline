a65d416ff3a6313b032ee97b0a88fe161f744400
Joeran Bosma <joeran@bosma.co>
2022-06-20 21:07:43 +0200

    Semi supervised learning scripts (#7)
    * Adapted prepare data for semi-supervised learning
    * Included scripts used to generate the automatic annotations
    *Included evaluation metrics of automatic annotations

---
+++ src/picai_baseline/nnunet_semi_supervised/ensemble_predictions.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+from pathlib import Path
+from typing import List
+
+import numpy as np
+import SimpleITK as sitk
+from picai_baseline.splits.picai import train_splits, valid_splits
+from picai_eval.image_utils import read_prediction
+from picai_prep.data_utils import atomic_image_write
+from picai_prep.preprocessing import resample_to_reference_scan
+from tqdm import tqdm
+
+from softmax_export import save_softmax_nifti_from_softmax
+
+nnUNet_results = Path("/workdir/results/nnUNet/3d_fullres/")
+in_dir_scans_original = Path("/input/images")
+in_dir_scans_cropped = Path("/workdir/")
+nnUNet_task = nnUNet_results / "Task2201_picai_baseline/nnUNetTrainerV2_Loss_FL_and_CE_checkpoints__nnUNetPlansv2.1"
+out_dir_pred = nnUNet_task / "picai_pubtrain_predictions_model_best_ensemble"
+in_dir_pred = [
+    nnUNet_task / f"fold_{fold}/picai_pubtrain_predictions_model_best"
+    for fold in range(5)
+]
+in_dir_properties = in_dir_pred[1]
+subject_list: List[str] = train_splits[0]["subject_list"] + valid_splits[0]["subject_list"]
+
+out_dir_pred.mkdir(exist_ok=True)
+
+# ensemble model predictions from 5-fold cross-validation
+for subject_id in tqdm(subject_list):
+    out_path_softmax = out_dir_pred / f"{subject_id}.nii.gz"
+    if out_path_softmax.exists():
+        pass # continue
+
+    # read predictions from all folds
+    preds = [read_prediction(in_dir / f"{subject_id}.npz") for in_dir in in_dir_pred]
+
+    # average predictions
+    pred = np.mean(preds, axis=0)
+
+    # convert to nnUNet's format
+    pred = np.array([1-pred, pred])
+
+    # read physical properties of current case
+    properties_path = in_dir_properties / f"{subject_id}.pkl"
+    with open(properties_path, "rb") as fp:
+        properties = pickle.load(fp)
+
+    # let nnUNet resample to original physical space
+    out_path_softmax = out_dir_pred / f"{subject_id}.nii.gz"
+    save_softmax_nifti_from_softmax(
+        segmentation_softmax=pred,
+        out_fname=str(out_path_softmax),
+        properties_dict=properties,
+    )
+
+"""
+docker run --cpus=4 --memory=32gb --rm \
+    -v /path/to/workdir:/workdir/ \
+    -v /path/to/repos:/repos/ \
+    -v /data/central/pelvis/data/prostate-MRI/picai/public_training/images/:/input/images/ \
+    joeranbosma/picai_nnunet:latest python /repos/picai_baseline/src/picai_baseline/nnunet/ensemble_predictions.py
+"""
