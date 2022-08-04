e82256541ba5b93adabe5d9da79f6c4a273493c5
anindo-saha <anindo@ieee.org>
2022-08-05 03:05:16 +0600

    os.sched_getaffinity(0) not supported by all operating systems

--- src/picai_baseline/unet/training_setup/compute_spec.py
+++ src/picai_baseline/unet/training_setup/compute_spec.py
@@ -20,7 +20,13 @@ import torch
 def compute_spec_for_run(args, train_device='cuda'):
     """Register GPU/CPU specifications"""
     device = torch.device(train_device)
-    args.num_threads = np.min([len(os.sched_getaffinity(0)), args.max_threads])
+    
+    # os.sched_getaffinity(0) is not supported by all operating systems
+    try:
+        args.num_threads = np.min([len(os.sched_getaffinity(0)), args.max_threads])
+    except:
+        args.num_threads = args.max_threads
+    
     print("-"*100)
     print("GPUs:", torch.cuda.get_device_name(0))
     print("CPU Threads:", args.num_threads)
