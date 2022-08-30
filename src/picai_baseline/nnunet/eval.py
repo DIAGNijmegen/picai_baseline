4abd5e87e0894de4d9a9d293b81d8c943d8c6555
Joeran Bosma <joeran@bosma.co>
2022-08-30 19:33:59 +0200

    Cleanup

--- src/picai_baseline/nnunet/eval.py
+++ src/picai_baseline/nnunet/eval.py
@@ -25,14 +25,14 @@ task = "Task2201_picai_baseline"
 trainer = "nnUNetTrainerV2_Loss_FL_and_CE_checkpoints__nnUNetPlansv2.1"
 task_dir = Path("/workdir/results/nnUNet/3d_fullres/") / task
 checkpoints = ["model_best"]
-
+threshold = "dynamic"  # use dynamic-fast for quicker evaluation at almost equal performance
 
 for fold in range(5):
     print(f"Fold {fold}")
 
     for checkpoint in checkpoints:
         softmax_dir = task_dir / trainer / f"fold_{fold}/picai_pubtrain_predictions_{checkpoint}"
-        metrics_path = softmax_dir.parent  / f"metrics-{checkpoint}-picai_eval-dynamic.json"
+        metrics_path = softmax_dir.parent  / f"metrics-{checkpoint}-picai_eval-{threshold}.json"
 
         if metrics_path.exists():
             print(f"Metrics found at {metrics_path}, skipping..")
@@ -51,7 +51,7 @@ for fold in range(5):
             y_true_dir=f"/workdir/nnUNet_raw_data/{task}/labelsTr",
             subject_list=valid_splits[fold]['subject_list'],
             pred_extensions=['_softmax.nii.gz'],
-            y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred, threshold="dynamic")[0],
+            y_det_postprocess_func=lambda pred: extract_lesion_candidates(pred, threshold=threshold)[0],
         )
 
         # save and show metrics
