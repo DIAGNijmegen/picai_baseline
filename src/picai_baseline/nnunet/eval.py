02598bf660942d6dfcc75bc563fde90ec63196f3
Joeran Bosma <joeran@bosma.co>
2022-08-31 14:40:35 +0200

    Documentation

--- src/picai_baseline/nnunet/eval.py
+++ src/picai_baseline/nnunet/eval.py
@@ -45,9 +45,9 @@ def evaluate(
     task_dir: Union[Path, str] = "auto",
     checkpoints: str = ["model_best"],
     folds: str = list(range(5)),
-    threshold: str = "dynamic",
     softmax_postprocessing_func: "Optional[Union[Callable[[npt.NDArray[np.float_]], npt.NDArray[np.float_]], str]]" = "extract_lesion_candidates",
-    metrics_fn: str = "metrics-{checkpoint}-picai_eval-{threshold}.json",
+    threshold: str = "dynamic",
+    metrics_fn: str = "metrics-{checkpoint}-{threshold}.json",
 ):
     # input validation
     workdir = Path(workdir)
@@ -101,16 +101,25 @@ def evaluate(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Command Line Arguments')
-    parser.add_argument("--task", type=str, default="Task2201_picai_baseline")
-    parser.add_argument("--trainer", type=str, default="nnUNetTrainerV2_Loss_FL_and_CE_checkpoints")
-    parser.add_argument("--workdir", type=str, default="/workdir")
-    parser.add_argument("--task_dir", type=str, default="auto")
-    parser.add_argument("--checkpoints", type=str, nargs="+", default=["model_best"])
-    parser.add_argument("--folds", type=int, nargs="+", default=list(range(5)))
+    parser.add_argument("--task", type=str, default="Task2201_picai_baseline",
+                        help="Task name of the nnU-Net experiment. Default: Task2201_picai_baseline")
+    parser.add_argument("--trainer", type=str, default="nnUNetTrainerV2_Loss_FL_and_CE_checkpoints",
+                        help="Trainer of the nnU-Net experiment. Default: nnUNetTrainerV2_Loss_FL_and_CE_checkpoints")
+    parser.add_argument("--workdir", type=str, default="/workdir",
+                        help="Path to the workdir where 'results' and 'nnUNet_raw_data' are stored. Default: /workdir")
+    parser.add_argument("--task_dir", type=str, default="auto",
+                        help="Path to the task directory (relative to the workdir). Optional, will be constucted for default nnU-Net forlder structure")
+    parser.add_argument("--checkpoints", type=str, nargs="+", default=["model_best"],
+                        help="Which checkpoints to evaluate. Multiple checkpoints can be passed at once. Default: model_best")
+    parser.add_argument("--folds", type=int, nargs="+", default=list(range(5)),
+                        help="Which folds to evaluate. Multiple folds can be evaluated at once. Default: 0, 1, 2, 3, 4  (all)")
+    parser.add_argument("--softmax_postprocessing_func", type=str, default="extract_lesion_candidates",
+                        help="Function to post-process the softmax volumes. Default: extract lesion candidates using the Report-Guided Annotation repository.")
     parser.add_argument("--threshold", type=str, default="dynamic",
                         help="Threshold for lesion extraction from softmax predictions. " + \
-                             "Use dynamic-fast for quicker evaluation at almost equal performance")
-    parser.add_argument("--metrics_fn", type=str, default="metrics-{checkpoint}-{threshold}.json")
+                             "Use dynamic-fast for quicker evaluation at almost equal performance.")
+    parser.add_argument("--metrics_fn", type=str, default=r"metrics-{checkpoint}-{threshold}.json",
+                        help=r"Filename to save metrics to. May contain {checkpoint} and {threshold} which are auto-filled. Default: metrics-{checkpoint}-{threshold}.json")
     args = parser.parse_args()
 
     # evaluate
@@ -121,6 +130,7 @@ if __name__ == "__main__":
         task_dir=args.task_dir,
         checkpoints=args.checkpoints,
         folds=args.folds,
+        softmax_postprocessing_func=args.softmax_postprocessing_func,
         threshold=args.threshold,
         metrics_fn=args.metrics_fn,
     )
--- src/picai_baseline/nnunet_semi_supervised/eval.py
+++ src/picai_baseline/nnunet_semi_supervised/eval.py
@@ -19,15 +19,23 @@ from picai_baseline.nnunet.eval import evaluate
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Command Line Arguments')
     parser.add_argument("--task", type=str, default="Task2203_picai_baseline")
-    parser.add_argument("--trainer", type=str, default="nnUNetTrainerV2_Loss_FL_and_CE_checkpoints")
-    parser.add_argument("--workdir", type=str, default="/workdir")
-    parser.add_argument("--task_dir", type=str, default="auto")
-    parser.add_argument("--checkpoints", type=str, nargs="+", default=["model_best"])
-    parser.add_argument("--folds", type=int, nargs="+", default=list(range(5)))
+    parser.add_argument("--trainer", type=str, default="nnUNetTrainerV2_Loss_FL_and_CE_checkpoints",
+                        help="Trainer of the nnU-Net experiment. Default: nnUNetTrainerV2_Loss_FL_and_CE_checkpoints")
+    parser.add_argument("--workdir", type=str, default="/workdir",
+                        help="Path to the workdir where 'results' and 'nnUNet_raw_data' are stored. Default: /workdir")
+    parser.add_argument("--task_dir", type=str, default="auto",
+                        help="Path to the task directory (relative to the workdir). Optional, will be constucted for default nnU-Net forlder structure")
+    parser.add_argument("--checkpoints", type=str, nargs="+", default=["model_best"],
+                        help="Which checkpoints to evaluate. Multiple checkpoints can be passed at once. Default: model_best")
+    parser.add_argument("--folds", type=int, nargs="+", default=list(range(5)),
+                        help="Which folds to evaluate. Multiple folds can be evaluated at once. Default: 0, 1, 2, 3, 4  (all)")
+    parser.add_argument("--softmax_postprocessing_func", type=str, default="extract_lesion_candidates",
+                        help="Function to post-process the softmax volumes. Default: extract lesion candidates using the Report-Guided Annotation repository.")
     parser.add_argument("--threshold", type=str, default="dynamic",
                         help="Threshold for lesion extraction from softmax predictions. " + \
-                             "Use dynamic-fast for quicker evaluation at almost equal performance")
-    parser.add_argument("--metrics_fn", type=str, default="metrics-{checkpoint}-{threshold}.json")
+                             "Use dynamic-fast for quicker evaluation at almost equal performance.")
+    parser.add_argument("--metrics_fn", type=str, default=r"metrics-{checkpoint}-{threshold}.json",
+                        help=r"Filename to save metrics to. May contain {checkpoint} and {threshold} which are auto-filled. Default: metrics-{checkpoint}-{threshold}.json")
     args = parser.parse_args()
 
     # evaluate
@@ -38,6 +46,7 @@ if __name__ == "__main__":
         task_dir=args.task_dir,
         checkpoints=args.checkpoints,
         folds=args.folds,
+        softmax_postprocessing_func=args.softmax_postprocessing_func,
         threshold=args.threshold,
         metrics_fn=args.metrics_fn,
     )
