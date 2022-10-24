6a0c4d753b4e498bb3e1abc03e7aa97474a55b2d
Joeran Bosma <joeran@bosma.co>
2022-10-24 10:26:32 +0200

    Convert U-Net plan_overview to a function

--- src/picai_baseline/unet/plan_overview.py
+++ src/picai_baseline/unet/plan_overview.py
@@ -14,64 +14,75 @@
 
 import json
 from pathlib import Path
+from typing import Dict, List, Optional, Union
 
 import numpy as np
 import SimpleITK as sitk
 from picai_baseline.splits.picai_nnunet import nnunet_splits
 
-# specify paths to preprocessed data in nnU-Net Raw Data Archive
-preprocessed_data_path = Path('/workdir/nnUNet_raw_data/Task2201_picai_baseline/')
-overviews_path = Path('/workdir/results/UNet/overviews/')
 
-# specify cases that should be excluded from the train and validation sets
-excluded_cases = [
-    '11475_1001499'  # cannot be preprocessed with the default baseline U-Net preprocessing config
-]
+def main(
+    preprocessed_data_path: Union[Path, str] = Path('/workdir/nnUNet_raw_data/Task2201_picai_baseline/'),
+    overviews_path: Union[Path, str] = Path('/workdir/results/UNet/overviews/'),
+    splits: Optional[Dict[str, List[str]]] = None,
+):
+    """Create overviews of the training data."""
 
-# create directory to store overviews
-overviews_path.mkdir(parents=True, exist_ok=True)
+    if splits is None:
+        splits = nnunet_splits
 
-# iterate over each cross-validation fold
-for fold, nnunet_fold in enumerate(nnunet_splits):
+    # specify cases that should be excluded from the train and validation sets
+    excluded_cases = [
+        '11475_1001499'  # cannot be preprocessed with the default baseline U-Net preprocessing config
+    ]
 
-    # iterate over train and validation splits
-    for split, nnunet_split in nnunet_fold.items():
-        print(f"Preparing fold {fold}..")
+    # create directory to store overviews
+    overviews_path.mkdir(parents=True, exist_ok=True)
 
-        # initialize list of fields to collect for each split of each fold
-        overview = {
-            'pat_ids': [],
-            'study_ids': [],
-            'image_paths': [],
-            'label_paths': [],
-            'case_label': [],
-            'ratio_csPCa_bg': []
-        }
+    # iterate over each cross-validation fold
+    for fold, nnunet_fold in enumerate(splits):
 
-        # iterate over each training/validation case
-        for subject_id in nnunet_split:
-            patient_id, study_id = subject_id.split('_')
+        # iterate over train and validation splits
+        for split, nnunet_split in nnunet_fold.items():
+            print(f"Preparing fold {fold}..")
 
-            # skip excluded case(s)
-            if subject_id in excluded_cases:
-                continue
+            # initialize list of fields to collect for each split of each fold
+            overview = {
+                'pat_ids': [],
+                'study_ids': [],
+                'image_paths': [],
+                'label_paths': [],
+                'case_label': [],
+                'ratio_csPCa_bg': []
+            }
 
-            # load annotation
-            lbl = sitk.GetArrayFromImage(sitk.ReadImage(str(preprocessed_data_path / 'labelsTr' / f'{subject_id}.nii.gz')))
+            # iterate over each training/validation case
+            for subject_id in nnunet_split:
+                patient_id, study_id = subject_id.split('_')
 
-            overview['pat_ids'] += [patient_id]
-            overview['study_ids'] += [study_id]
-            overview['image_paths'] += [[
-                str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0000.nii.gz').as_posix()),
-                str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0001.nii.gz').as_posix()),
-                str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0002.nii.gz').as_posix()),
-            ]]
-            overview['label_paths'] += [str((preprocessed_data_path / 'labelsTr' / f'{subject_id}.nii.gz').as_posix())]
-            overview['case_label'] += [float(np.max(lbl))]
-            overview['ratio_csPCa_bg'] += [float(np.sum(lbl)/np.size(lbl))]
+                # skip excluded case(s)
+                if subject_id in excluded_cases:
+                    continue
 
-        # save overview
-        with open(overviews_path / f'PI-CAI_{split}-fold-{fold}.json', 'w') as fp:
-            json.dump(overview, fp, indent=4)
+                # load annotation
+                lbl = sitk.GetArrayFromImage(sitk.ReadImage(str(preprocessed_data_path / 'labelsTr' / f'{subject_id}.nii.gz')))
 
-print("Finished.")
+                overview['pat_ids'] += [patient_id]
+                overview['study_ids'] += [study_id]
+                overview['image_paths'] += [[
+                    str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0000.nii.gz').as_posix()),
+                    str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0001.nii.gz').as_posix()),
+                    str((preprocessed_data_path / 'imagesTr' / f'{subject_id}_0002.nii.gz').as_posix()),
+                ]]
+                overview['label_paths'] += [str((preprocessed_data_path / 'labelsTr' / f'{subject_id}.nii.gz').as_posix())]
+                overview['case_label'] += [float(np.max(lbl))]
+                overview['ratio_csPCa_bg'] += [float(np.sum(lbl)/np.size(lbl))]
+
+            # save overview
+            with open(overviews_path / f'PI-CAI_{split}-fold-{fold}.json', 'w') as fp:
+                json.dump(overview, fp, indent=4)
+
+
+if __name__ == '__main__':
+    main()
+    print("Finished.")
