f4daa011cc6b21b69bb5bc5874550e3f439fda07
Joeran Bosma <joeran@bosma.co>
2022-10-27 12:06:19 +0200

    PI-CAI PubPrivTrain cross-validation splits

---
+++ src/picai_baseline/splits/picai_pubpriv/__main__.py
@@ -0,0 +1,8 @@
+from picai_baseline.splits import export_splits
+from picai_baseline.splits.picai_nnunet import train_splits, valid_splits
+
+if __name__ == "__main__":
+    export_splits(
+        train_splits=train_splits,
+        valid_splits=valid_splits,
+    )
