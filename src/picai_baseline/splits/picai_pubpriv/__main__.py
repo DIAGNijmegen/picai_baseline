ef10e5cd8ef4785814c8ee42a23ece776c5340ea
Joeran Bosma <joeran@bosma.co>
2022-10-31 11:58:25 +0100

    Bugfix

--- src/picai_baseline/splits/picai_pubpriv/__main__.py
+++ src/picai_baseline/splits/picai_pubpriv/__main__.py
@@ -1,5 +1,5 @@
 from picai_baseline.splits import export_splits
-from picai_baseline.splits.picai_nnunet import train_splits, valid_splits
+from picai_baseline.splits.picai_pubpriv import train_splits, valid_splits
 
 if __name__ == "__main__":
     export_splits(
--- src/picai_baseline/splits/picai_pubpriv_nnunet/__main__.py
+++ src/picai_baseline/splits/picai_pubpriv_nnunet/__main__.py
@@ -1,5 +1,6 @@
 from picai_baseline.splits import export_splits
-from picai_baseline.splits.picai_nnunet import train_splits, valid_splits
+from picai_baseline.splits.picai_pubpriv_nnunet import (train_splits,
+                                                        valid_splits)
 
 if __name__ == "__main__":
     export_splits(
