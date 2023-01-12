eb3a45650deb91c578cc1742b0c4ff26db056c16
Joeran Bosma <joeran@bosma.co>
2023-01-12 15:29:57 +0100

    Debug scripts

---
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_debug.sh
@@ -0,0 +1,7 @@
+docker run --cpus=8 --memory=16g --shm-size=16g --rm -it \
+    -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code \
+    -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
+    -v /media/pelvis/data/prostate-MRI/picai/debug:/input \
+    -v /media/pelvis/projects/joeran/picai/debug-output:/output \
+    joeranbosma/picai_nnunet:latest \
+    python /code/preprocess.py --splits picai_debug
