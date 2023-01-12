39ca5e11c4a9799dfc9e250189f52ed03d9f4da6
Joeran Bosma <joeran@bosma.co>
2023-01-12 14:48:37 +0100

    Allow supervised preprocessing from cmd line

--- src/picai_baseline/__main__.py
+++ src/picai_baseline/__main__.py
@@ -15,6 +15,7 @@
 import argparse
 import os
 
+from picai_baseline.prepare_data import prepare_data
 from picai_baseline.prepare_data_semi_supervised import \
     prepare_data_semi_supervised
 
@@ -44,6 +45,30 @@ if __name__ == '__main__':
                         help="Splits to save for cross-validation. Available: picai_pub, picai_pubpriv.")
     parser.add_argument("--task", type=str, default="Task2203_picai_baseline",
                         help="Task name (default: Task2203_picai_baseline)")
+    parser.set_defaults(func=prepare_data_semi_supervised)
+
+    parser = subparsers.add_parser('prepare_data')
+    parser.add_argument("--workdir", type=str, default=os.environ.get("workdir", "/workdir"),
+                        help="Path to the working directory (default: /workdir, or the environment variable 'workdir')")
+    parser.add_argument("--inputdir", type=str, default=os.environ.get("inputdir", "/input"),
+                        help="Path to the input dataset (default: /input, or the environment variable 'inputdir')")
+    parser.add_argument("--imagesdir", type=str, default="images",
+                        help="Path to the images, relative to --inputdir (default: /input/images)")
+    parser.add_argument("--labelsdir", type=str, default="picai_labels",
+                        help="Path to the labels, relative to --inputdir (root of picai_labels) (default: /input/picai_labels)")
+    parser.add_argument("--spacing", type=float, nargs="+", required=False,
+                        help="Spacing to preprocess images to. Default: keep as-is.")
+    parser.add_argument("--matrix_size", type=int, nargs="+", required=False,
+                        help="Matrix size to preprocess images to. Default: keep as-is.")
+    parser.add_argument("--preprocessing_kwargs", type=str, required=False,
+                        help='Preprocessing kwargs to pass to the MHA2nnUNetConverter. " + \
+                            "E.g.: `{"crop_only": true}`. Must be valid json.')
+    parser.add_argument("--splits", type=str, default="picai_pub_nnunet",
+                        help="Splits to save for cross-validation. Available: picai_pub_nnunet, picai_pubpriv_nnunet.")
+    parser.add_argument("--task", type=str, default="Task2201_picai_baseline",
+                        help="Task name (default: Task2201_picai_baseline)")
+    parser.set_defaults(func=prepare_data)
+
     try:
         args = argparser.parse_args()
     except Exception as e:
@@ -51,7 +76,7 @@ if __name__ == '__main__':
         print("Retrying with only the known arguments...")
         args, _ = argparser.parse_known_args()
 
-    prepare_data_semi_supervised(
+    args.func(
         workdir=args.workdir,
         inputdir=args.inputdir,
         imagesdir=args.imagesdir,
