65a7754ed2ba1f28423fadab3769f1918efb96d5
Joeran Bosma <joeran@bosma.co>
2023-01-12 15:33:23 +0100

    Increase memory

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
@@ -1,4 +1,4 @@
-docker run --cpus=8 --memory=16g --shm-size=16g --rm -it \
+docker run --cpus=8 --memory=32g --shm-size=32g --rm -it \
     -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code \
     -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
     -v /media/pelvis/data/prostate-MRI/picai/public_training:/input \
