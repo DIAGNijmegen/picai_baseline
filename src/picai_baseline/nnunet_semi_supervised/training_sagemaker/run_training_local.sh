2538960ef029877ae9dec77ca36f5ef320e41b51
Joeran Bosma <joeran@bosma.co>
2023-01-13 18:54:21 +0100

    Update run_training_local.sh

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
@@ -1,7 +1,7 @@
-docker run --cpus=8 --memory=32g --shm-size=32g --rm -it \
-    -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code \
-    -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
-    -v /media/pelvis/data/prostate-MRI/picai/debug:/input \
-    -v /media/pelvis/projects/joeran/picai/debug-output:/input/preprocessed \
+docker run --cpus=8 --memory=61g --shm-size=61g --gpus='"device=0"' --rm -it \
+    -v /mnt/netcache/pelvis/projects/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-workdir:/workdir \
+    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug:/input \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-output:/input/preprocessed \
     joeranbosma/picai_nnunet:latest \
     python /code/train.py
