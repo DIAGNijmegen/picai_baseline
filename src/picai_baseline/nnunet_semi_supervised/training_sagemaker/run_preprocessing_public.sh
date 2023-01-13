27f5f5869a2413e06dd3d13a1a0f872c7dcfc36c
Joeran Bosma <joeran@bosma.co>
2023-01-13 19:16:14 +0100

    Update run_preprocessing_public.sh

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
@@ -1,8 +1,8 @@
 docker run --cpus=8 --memory=32g --shm-size=32g --rm -it \
-    -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
-    -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
-    -v /media/pelvis/data/prostate-MRI/picai/public_training:/input:ro \
-    -v /media/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
-    -v /media/pelvis/projects/joeran/picai/debug-output:/output \
+    -v /mnt/netcache/pelvis/projects/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-workdir:/workdir \
+    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/public_training:/input:ro \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-output:/output \
     joeranbosma/picai_nnunet:latest \
     python /code/preprocess.py --splits picai_pub
