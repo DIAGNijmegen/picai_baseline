51c9e7bdc1b799cca44562ad6eb957682ce811f3
Joeran Bosma <joeran@bosma.co>
2023-01-13 19:13:18 +0100

    Mount a folder for the checkpoints

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_public.sh
@@ -2,6 +2,7 @@ docker run --cpus=8 --memory=32g --shm-size=32g --rm -it \
     -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
     -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
     -v /media/pelvis/data/prostate-MRI/picai/public_training:/input:ro \
+    -v /media/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
     -v /media/pelvis/projects/joeran/picai/debug-output:/output \
     joeranbosma/picai_nnunet:latest \
     python /code/preprocess.py --splits picai_pub
