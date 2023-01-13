4974cc1543e0d1141323d5a44f47a3c291ab58a1
Joeran Bosma <joeran@bosma.co>
2023-01-13 19:11:34 +0100

    Make input directories read-only

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
@@ -1,7 +1,7 @@
 docker run --cpus=8 --memory=61g --shm-size=61g --gpus='"device=0"' --rm -it \
-    -v /mnt/netcache/pelvis/projects/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code \
+    -v /mnt/netcache/pelvis/projects/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
     -v /mnt/netcache/pelvis/projects/joeran/picai/debug-workdir-training:/workdir \
-    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug:/input \
-    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-output:/input/preprocessed \
+    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug:/input:ro \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-output:/input/preprocessed:ro \
     joeranbosma/picai_nnunet:latest \
     python /code/train.py
