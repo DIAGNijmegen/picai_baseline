bf87a4c6dd847b958d4bacfb739e571808848c13
Joeran Bosma <joeran@bosma.co>
2023-01-15 20:09:23 +0100

    Update run_training_local.sh

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_training_local.sh
@@ -5,5 +5,6 @@ docker run --cpus=8 --memory=61g --shm-size=61g --gpus='"device=0"' --rm -it \
     -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug/picai_labels:/input/picai_labels:ro \
     -v /mnt/netcache/pelvis/projects/joeran/picai/debug-preprocessed:/input/preprocessed:ro \
     -v /mnt/netcache/pelvis/projects/joeran/picai/debug-output:/output \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
     joeranbosma/picai_nnunet:latest \
     python /code/train.py
