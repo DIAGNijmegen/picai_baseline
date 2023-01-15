2b47f28bf02c1868ddd2b4ad461ce65cd3ac8f07
Joeran Bosma <joeran@bosma.co>
2023-01-15 16:41:40 +0100

    Cleanup paths

--- src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_debug.sh
+++ src/picai_baseline/nnunet_semi_supervised/training_sagemaker/run_preprocessing_debug.sh
@@ -1,9 +1,9 @@
 docker run --cpus=8 --memory=32g --shm-size=32g --rm -it \
-    -v ~/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
-    -v /media/pelvis/projects/joeran/picai/debug-workdir:/workdir \
-    -v /media/pelvis/data/prostate-MRI/picai/debug/images:/input/images:ro \
-    -v /media/pelvis/data/prostate-MRI/picai/debug/picai_labels:/input/picai_labels:ro \
-    -v /media/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
-    -v /media/pelvis/projects/joeran/picai/debug-preprocessed:/output \
+    -v /mnt/netcache/pelvis/projects/joeran/repos/picai_baseline/src/picai_baseline/nnunet_semi_supervised/training_sagemaker/code:/code:ro \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-workdir:/workdir \
+    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug/images:/input/images:ro \
+    -v /mnt/netcache/pelvis/data/prostate-MRI/picai/debug/picai_labels:/input/picai_labels:ro \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-checkpoints:/checkpoints \
+    -v /mnt/netcache/pelvis/projects/joeran/picai/debug-preprocessed:/output \
     joeranbosma/picai_nnunet:latest \
     python /code/preprocess.py --splits picai_debug
