e8ea6fd0b4508078cf7d574bea2abd27ad0c8e71
Joeran Bosma <joeran@bosma.co>
2024-05-13 15:15:33 +0200

    Update version picai_baseline
    and add it to the UNet requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         long_description = fh.read()
 
     setuptools.setup(
-        version='0.8.4',
+        version='0.8.5',
         author_email='Joeran.Bosma@radboudumc.nl',
         long_description=long_description,
         long_description_content_type="text/markdown",
